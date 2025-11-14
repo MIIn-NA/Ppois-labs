ab4aad949ad5fb199258a8a9fecc87242718a6d0
MIIn-NA <foxy.lisaa06@gmail.com>
2025-11-14 20:08:34 +0300

    Delete lab1 directory

--- lab1/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/markov-algorithm
-# Build directory: /home/ubuntu/markov-algorithm/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

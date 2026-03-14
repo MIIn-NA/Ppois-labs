3819441edd515b4b519618e747309100d55e33c5
MIIn-NA <foxy.lisaa06@gmail.com>
2026-03-14 11:04:24 +0300

    Update binary_operations.cpp

--- aoislab1/binary_operations.cpp
+++ aoislab1/binary_operations.cpp
@@ -61,12 +61,12 @@ BinaryArray twos_complement(const BinaryArray& arr) {
     
     BinaryArray res = arr;
     
-    // Инвертируем биты
+    
     for (int i = 1; i < NUMB_OF_BITS; i++) {
         res[i] = 1 - res[i];
     }
     
-    // Добавляем 1
+
     for (int i = NUMB_OF_BITS - 1; i > 0; i--) {
         res[i] += 1;
         if (res[i] == 2) {
@@ -109,7 +109,7 @@ BinaryArray multiply(int x1, int x2) {
     BinaryArray bin2 = decimal_to_binary(x2);
     
     BinaryArray ans = {0};
-    ans[0] = bin1[0] ^ bin2[0];  // XOR для знака
+    ans[0] = bin1[0] ^ bin2[0];  
     
     vector<int> temp_ans(NUMB_OF_BITS * 2, 0);
     
@@ -125,7 +125,7 @@ BinaryArray multiply(int x1, int x2) {
         }
     }
     
-    // Обработка переносов
+    
     for (int i = NUMB_OF_BITS - 1; i > 0; i--) {
         int carry = temp_ans[i] / 2;
         temp_ans[i] %= 2;
@@ -134,7 +134,7 @@ BinaryArray multiply(int x1, int x2) {
         }
     }
     
-    // Копируем результат в ans (биты с 1 по 31)
+    
     for (int i = 1; i < NUMB_OF_BITS; i++) {
         ans[i] = temp_ans[i];
     }
@@ -169,7 +169,7 @@ DynamicBinary subtract_binary(const DynamicBinary& a, const DynamicBinary& b) {
     DynamicBinary a_clean = clean_zeros(a);
     DynamicBinary b_clean = clean_zeros(b);
     
-    // Выравниваем длины
+    
     while (b_clean.size() < a_clean.size()) {
         b_clean.insert(b_clean.begin(), 0);
     }
@@ -199,14 +199,14 @@ tuple<int, DynamicBinary, DynamicBinary> divide(int x1, int x2) {
     BinaryArray x1_bin = decimal_to_binary(x1);
     BinaryArray x2_bin = decimal_to_binary(x2);
     
-    // Знак результата - XOR знаков
+    
     int sign = x1_bin[0] ^ x2_bin[0];
     
-    // Берем модули чисел (убираем знаковый бит)
+
     DynamicBinary x1_mag(x1_bin.begin() + 1, x1_bin.end());
     DynamicBinary x2_mag(x2_bin.begin() + 1, x2_bin.end());
     
-    // Удаляем ведущие нули
+    
     x1_mag = clean_zeros(x1_mag);
     x2_mag = clean_zeros(x2_mag);
     
@@ -214,12 +214,12 @@ tuple<int, DynamicBinary, DynamicBinary> divide(int x1, int x2) {
     DynamicBinary float_part;
     DynamicBinary remainder;
     
-    // Особый случай: деление на 1
+    
     if (x2_mag.size() == 1 && x2_mag[0] == 1) {
         integer_part = x1_mag;
         if (integer_part.empty()) integer_part = {0};
         
-        // Дробная часть - все нули
+    
         for (int i = 0; i < 17; i++) {
             float_part.push_back(0);
         }
@@ -227,7 +227,7 @@ tuple<int, DynamicBinary, DynamicBinary> divide(int x1, int x2) {
         return make_tuple(sign, integer_part, float_part);
     }
     
-    // Особый случай: делимое равно 0
+    
     if (x1_mag.size() == 1 && x1_mag[0] == 0) {
         integer_part = {0};
         for (int i = 0; i < 17; i++) {
@@ -236,19 +236,18 @@ tuple<int, DynamicBinary, DynamicBinary> divide(int x1, int x2) {
         return make_tuple(sign, integer_part, float_part);
     }
     
-    // Случай 1: делимое меньше делителя
+    
     if (!is_greater_or_equal(x1_mag, x2_mag)) {
-        integer_part = {0};  // Целая часть = 0
-        remainder = x1_mag;   // Остаток = делимое
+        integer_part = {0}; 
+        remainder = x1_mag; 
         
-        // Вычисляем дробную часть (17 бит точности)
+       
         for (int i = 0; i < 17; i++) {
             if (remainder.size() == 1 && remainder[0] == 0) {
                 float_part.push_back(0);
                 continue;
             }
             
-            // Умножаем остаток на 2 (добавляем 0 в конец)
             remainder.push_back(0);
             remainder = clean_zeros(remainder);
             
@@ -263,8 +262,7 @@ tuple<int, DynamicBinary, DynamicBinary> divide(int x1, int x2) {
         return make_tuple(sign, integer_part, float_part);
     }
     
-    // Случай 2: обычное деление
-    // Целая часть
+   
     for (int bit : x1_mag) {
         remainder.push_back(bit);
         remainder = clean_zeros(remainder);
@@ -283,14 +281,14 @@ tuple<int, DynamicBinary, DynamicBinary> divide(int x1, int x2) {
         integer_part.push_back(0);
     }
     
-    // Дробная часть (17 бит точности)
+ 
     for (int i = 0; i < 17; i++) {
         if (remainder.size() == 1 && remainder[0] == 0) {
             float_part.push_back(0);
             continue;
         }
         
-        // Умножаем остаток на 2
+        
         remainder.push_back(0);
         remainder = clean_zeros(remainder);
         

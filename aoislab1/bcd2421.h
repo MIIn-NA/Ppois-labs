840010fe0645b77673572538e7472c39b13d746e
MIIn-NA <foxy.lisaa06@gmail.com>
2026-03-14 11:02:41 +0300

    Update bcd2421.h

--- aoislab1/bcd2421.h
+++ aoislab1/bcd2421.h
@@ -8,15 +8,15 @@
 
 using namespace std;
 
-using Tetrad = vector<int>;  // 4 бита для одной десятичной цифры
-using BCDNumber = vector<Tetrad>;  // Массив тетрад
+using Tetrad = vector<int>;  
+using BCDNumber = vector<Tetrad>;  
 
-// Основные функции
+
 BCDNumber int_to_2421_array(int num);
 int array_2421_to_int(const BCDNumber& arr);
 BCDNumber add_2421_numbers(const BCDNumber& num1, const BCDNumber& num2);
 
-// Вспомогательные функции
+
 pair<int, Tetrad> add_4bit_binary(const Tetrad& a, const Tetrad& b, int cin = 0);
 pair<int, Tetrad> add_2421_digit(const Tetrad& digit1, const Tetrad& digit2, int carry_in = 0);
 string tetrad_to_string(const Tetrad& t);
